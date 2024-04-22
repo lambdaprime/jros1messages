7e3e4fcde71bb89f92e5b54b296abfdcae3b0601
lambdaprime <intid@protonmail.com>
2024-04-22 03:29:46 +0000

    Adding more tests

--- jros1messages.tests/src/test/java/id/jros1messages/tests/JRos1MessageTests.java
+++ jros1messages.tests/src/test/java/id/jros1messages/tests/JRos1MessageTests.java
@@ -20,8 +20,10 @@ package id.jros1messages.tests;
 import id.jros1messages.Ros1MessageSerializationUtils;
 import id.jros1messages.geometry_msgs.PolygonStampedMessage;
 import id.jros1messages.sensor_msgs.JointStateMessage;
+import id.jros1messages.sensor_msgs.MultiDOFJointStateMessage;
 import id.jros1messages.sensor_msgs.PointCloud2Message;
 import id.jros1messages.std_msgs.HeaderMessage;
+import id.jros1messages.trajectory_msgs.JointTrajectoryMessage;
 import id.jros1messages.vision_msgs.ObjectHypothesisWithPoseMessage;
 import id.jros1messages.visualization_msgs.MarkerArrayMessage;
 import id.jros1messages.visualization_msgs.MarkerMessage;
@@ -31,7 +33,10 @@ import id.jrosmessages.geometry_msgs.PolygonMessage;
 import id.jrosmessages.geometry_msgs.PoseMessage;
 import id.jrosmessages.geometry_msgs.PoseWithCovarianceMessage;
 import id.jrosmessages.geometry_msgs.QuaternionMessage;
+import id.jrosmessages.geometry_msgs.TransformMessage;
+import id.jrosmessages.geometry_msgs.TwistMessage;
 import id.jrosmessages.geometry_msgs.Vector3Message;
+import id.jrosmessages.geometry_msgs.WrenchMessage;
 import id.jrosmessages.primitives.Duration;
 import id.jrosmessages.primitives.Time;
 import id.jrosmessages.sensor_msgs.PointFieldMessage;
@@ -39,6 +44,7 @@ import id.jrosmessages.sensor_msgs.PointFieldMessage.DataType;
 import id.jrosmessages.std_msgs.ColorRGBAMessage;
 import id.jrosmessages.std_msgs.StringMessage;
 import id.jrosmessages.tests.MessageTests;
+import id.jrosmessages.trajectory_msgs.JointTrajectoryPointMessage;
 import java.util.stream.Stream;
 
 public class JRos1MessageTests extends MessageTests {
@@ -208,6 +214,73 @@ public class JRos1MessageTests extends MessageTests {
                                                         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                                                         14, 15.56, 16, 17, 18, 19, 20, 21, 22, 23,
                                                         24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
-                                                        35, 36))));
+                                                        35, 36))),
+                /*
+                 *
+                rostopic pub -r 10 wrench "sensor_msgs/MultiDOFJointState" '
+                header:
+                  seq: 12
+                  stamp: 0
+                  frame_id: "aaa"
+                joint_names: ["joint1", "joint2"]
+                transforms:
+                  - translation: [1,2,3]
+                    rotation: [4,5,6,7]
+                  - translation: [8,9,10]
+                    rotation: [11,12,13,14]
+                twist:
+                  - linear: [11,12,13]
+                    angular: [14,15,16]
+                  - linear: [18,19,110]
+                    angular: [111,112,113]
+                wrench:
+                  - force: [11,12,13]
+                    torque: [14,15,16]
+                  - force: [18,19,110]
+                    torque: [111,112,113]
+                '
+                *
+                */
+                new TestCase(
+                        "MultiDOFJointState",
+                        new MultiDOFJointStateMessage()
+                                .withHeader(new HeaderMessage().withSeq(12).withFrameId("aaa"))
+                                .withJointNames(
+                                        new StringMessage("joint1"), new StringMessage("joint2"))
+                                .withTransforms(
+                                        new TransformMessage()
+                                                .withTranslation(new Vector3Message(1, 2, 3))
+                                                .withRotation(
+                                                        new QuaternionMessage(4., 5., 6., 7.)),
+                                        new TransformMessage()
+                                                .withTranslation(new Vector3Message(8, 9, 10))
+                                                .withRotation(
+                                                        new QuaternionMessage(11, 12, 13, 14)))
+                                .withTwist(
+                                        new TwistMessage()
+                                                .withLinear(new Vector3Message(11, 12, 13))
+                                                .withAngular(new Vector3Message(14, 15, 16)),
+                                        new TwistMessage()
+                                                .withLinear(new Vector3Message(18, 19, 110))
+                                                .withAngular(new Vector3Message(111, 112, 113)))
+                                .withWrench(
+                                        new WrenchMessage()
+                                                .withForce(new Vector3Message(11, 12, 13))
+                                                .withTorque(new Vector3Message(14, 15, 16)),
+                                        new WrenchMessage()
+                                                .withForce(new Vector3Message(18, 19, 110))
+                                                .withTorque(new Vector3Message(111, 112, 113)))),
+                new TestCase(
+                        "JointTrajectory",
+                        new JointTrajectoryMessage()
+                                .withHeader(new HeaderMessage().withSeq(12).withFrameId("aaa"))
+                                .withJointNames(
+                                        new StringMessage("joint1"), new StringMessage("joint2"))
+                                .withPoints(
+                                        new JointTrajectoryPointMessage()
+                                                .withPositions(0x11, 0x12, 0x13, 0x14)
+                                                .withVelocities(0x11, 0x12, 0x13, 0x14)
+                                                .withAccelerations(0x11, 0x12, 0x13, 0x14)
+                                                .withEffort(0x11, 0x12, 0x13, 0x14))));
     }
 }
